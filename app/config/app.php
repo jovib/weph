6bc984c43e55a382400595c98ffa5c1ff4c5bba9
Joel Villalobos Batista <jovib29@gmail.com>
2014-06-27 09:54:06 -0500

    solve issue time zone

--- app/config/app.php
+++ app/config/app.php
@@ -39,7 +39,7 @@ return array(
 	|
 	*/
 
-	'timezone' => 'America/Panama_City',
+	'timezone' => 'America/Panama',
 
 	/*
 	|--------------------------------------------------------------------------
