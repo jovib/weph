3d5d9836778fbfd10fdf70d4bb15aaa6a477eab8
Joel Villalobos Batista <jovib29@gmail.com>
2014-07-07 11:09:34 -0500

    work of link create

--- app/views/layouts/sidebar.blade.php
+++ app/views/layouts/sidebar.blade.php
@@ -1,6 +1,8 @@
 <div class="col-sm-3 col-md-2 sidebar">
 	<ul class="nav nav-sidebar">
 		<li @yield('phone-active')><a href="{{URL::to('/phone')}}">Directorio</a></li>
+		<ul class="nav nav-sidebar">
 		@yield('phone-create')
+		</ul>
 	</ul>
 </div>
