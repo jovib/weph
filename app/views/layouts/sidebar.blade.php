368df6d14421d55d36c58bbf05ed8aa4c3d46373
Joel Villalobos Batista <jovib29@gmail.com>
2014-07-07 11:27:04 -0500

    update link create

--- app/views/layouts/sidebar.blade.php
+++ app/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="col-sm-3 col-md-2 sidebar">
 	<ul class="nav nav-sidebar">
 		<li @yield('phone-active')><a href="{{URL::to('/phone')}}">Directorio</a></li>
-		<ul class="nav nav-sidebar">
+		<ul class="nav">
 		@yield('phone-create')
 		</ul>
 	</ul>
