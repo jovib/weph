4d9655ba50756de3b56ec2f594681376910bec2e
Joel Villalobos Batista <jovib29@gmail.com>
2014-09-12 12:10:31 -0500

    testing first change ver:1.2

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,10 +2,10 @@
 <html lang="es">
 <head>
 	<meta charset ="utf-8">
-	<title>@yield('title') - Phonebook</title>
+	<title>@yield('title') - WePh</title>
 	{{HTML::style('//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css')}}
 	{{HTML::style('css/app.css')}}
-	{{HTML::style('http://fonts.googleapis.com/css?family=Lato')}}
+	{{HTML::style('http://fonts.googleapis.com/css?family=Ubuntu')}}
 </head>
 <body>
 {{HTML::script('js/jquery1-11-0.min.js')}}
--- app/views/layouts/navbar.blade.php
+++ app/views/layouts/navbar.blade.php
@@ -8,7 +8,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="{{URL::to('/')}}">Phonebook</a>
+			<a class="navbar-brand" href="{{URL::to('/')}}">WePh</a>
 		</div>
 		<div class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-right">
